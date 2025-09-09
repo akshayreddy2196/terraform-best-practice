2b05225d50075ac172c90d58f321854df58b11d2
akshayreddy2196 <77155965+akshayreddy2196@users.noreply.github.com>
2025-09-09 23:24:37 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,3 +1,10 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
 resource "aws_instance" "this" {
   ami           = "ami-0c02fb55956c7d316"
   instance_type = "t3.micro"
